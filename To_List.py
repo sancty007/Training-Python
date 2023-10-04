e7ae1801ebb0c9d5643010c64616861a7150634d
sancty <sanctifieryawmensah55@gmail.com>
2023-10-04 15:10:49 +0100

    essaie1

--- To_List.py
+++ To_List.py
@@ -16,7 +16,7 @@ def ajouter_tache():
             continue     
         else :
             print("Reponse invalide ")  
-    Affiche_menu()# Affiche du menu Principal
+    Affiche_menu()
             
 def afficher_taches():
     if len(taches) == 0 :
